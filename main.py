fd87109832252fe977c46d39988d57fd2797a488
AgustinAravena <114199177+AgustinAravena@users.noreply.github.com>
2025-08-12 04:58:18 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import base64
+import io
+from fastapi import FastAPI, Request
+import easyocr
+from PIL import Image
+
+app = FastAPI()
+# Carga los idiomas que necesitas. 'es' para español, 'en' para inglés.
+reader = easyocr.Reader(['es', 'en'])
+
+@app.post("/ocr")
+async def ocr_from_base64(request: Request):
+    try:
+        body = await request.json()
+        image_base64 = body.get("image")
+        if not image_base64:
+            return {"error": "El campo 'image' es requerido en el cuerpo de la solicitud."}
+
+        # Decodifica la imagen Base64 a datos binarios
+        image_data = base64.b64decode(image_base64)
+        
+        # Abre la imagen con la biblioteca PIL
+        img = Image.open(io.BytesIO(image_data))
+        
+        # Realiza el OCR en la imagen
+        result = reader.readtext(img)
+        
+        # Extrae solo el texto del resultado del OCR y únelo en una sola cadena
+        extracted_text = [text for (bbox, text, prob) in result]
+        full_text = " ".join(extracted_text)
+        
+        return {"text": full_text}
+    except Exception as e:
+        return {"error": str(e)}
